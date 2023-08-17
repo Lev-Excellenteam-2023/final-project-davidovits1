da1850f4fbd14a686c1ea80b5b6448e05e7aa91d
davidovits1 <david.o3111@gmail.com>
2023-08-17 15:09:15 +0300

    I added documentation for the new functions

--- json_file.py
+++ json_file.py
@@ -36,12 +36,34 @@ def crate_path_to_json(path: str, output_folder: str) -> str:
 
 
 def read_from_json(output_path: str) -> list:
+    """
+    This function reads data from a JSON file located at the given
+     output_path and returns the parsed data as a Python list.
+
+    Parameters:
+
+    output_path (str): The path to the JSON file to be read.
+    Returns:
+
+    output_data (list): The parsed data from the JSON file.
+
+    """
     with open(output_path, 'r') as json_file:
         output_data = json.load(json_file)
     return output_data
 
 
 def sort_json_to_send(output_data: list) -> Response:
+    """
+    This function takes a list of data, sorts it, and creates a Flask Response object containing the sorted JSON data.
+    
+    Parameters:
+
+    output_data (list): The list of data to be sent as JSON response.
+    Returns:
+
+    response (Response): A Flask Response object containing sorted JSON data.
+    """
     response = Response(
         json.dumps(output_data, indent=4, sort_keys=False),
         content_type='application/json'
